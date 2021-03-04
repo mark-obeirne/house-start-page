e2269a48c390bb15feb42da64f5f99ecf6b7d83a
Mark O'Beirne <markobeirne00@gmail.com>
2021-03-04 16:58:41 +0000

    Add function to get geolocation
    - Send geolocation to index.js
    - Process this fetch POST request
    - Add response to console on client

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ const express = require("express");
 const app = express();
 app.listen(3000, () => console.log("Listening at 3000"));
 app.use(express.static("public"));
+
+// Set up server to accept incoming requests as JSON
+app.use(express.json({limit: "1mb"}));
+
+// Setting up POST request
+app.post("/api", (request, response) => {
+    console.log(request.body);
+    // response.end() - Necessary to do something to complete it
+    response.json({
+        status: "Success",
+        latitude: request.body.lat,
+        longitude: request.body.long
+    })
+})
